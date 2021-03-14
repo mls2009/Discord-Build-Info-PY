6e618107fa929c96d9f7d70d8fe239bfab9c73d5
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-03-13 19:24:27 -0800

    Redone some of the tests

--- tests/test.py
+++ tests/test.py
@@ -1,28 +1,8 @@
-# universal import
+# Universal import
 from discord_build_info_py import *
 
-# Client tests
-printClientData('canary')
-print(clientBuildDataJSON('canary'))
+# Main test
+print(getClientData('canary'))
 
 # JSON tests
 print(clientBuildDataJSON('canary'))
-
-"""
-These are deprecated
-"""
-# canary import
-# from discord_build_info_py import getCanaryData, printCanaryBuildInfo, writeCanaryDataToFile
-# ptb import
-# from discord_build_info_py import getPTBData, printPTBBuildInfo, writePTBDataToFile
-# stable import
-# from discord_build_info_py import getStableData, printStableBuildInfo, writeStableDataToFile
-
-# Canary Tests
-# printCanaryBuildInfo()
-
-# PTB tests
-# printPTBBuildInfo()
-
-# Stable tests
-# printStableBuildInfo()
