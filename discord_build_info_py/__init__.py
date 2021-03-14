1643f314cfa1e2cecb8df2c7c02dcda939040c10
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-03-13 19:24:56 -0800

    Removed unnecessary comments

--- discord_build_info_py/__init__.py
+++ discord_build_info_py/__init__.py
@@ -1,5 +1,2 @@
-# from .canaryBuildInfo import getCanaryData, printCanaryBuildInfo, writeCanaryDataToFile, canaryDataJSON
-# from .ptbBuildInfo import getPTBData, printPTBBuildInfo, writePTBDataToFile, ptbDataJSON
-# from .stableBuildInfo import getStableData, printStableBuildInfo, writeStableDataToFile, stableDataJSON
 from .clientInfo import getClientData, printClientData, writeClientDataToLocalFile, clientBuildDataJSON
 from .handler.parse import reparse
